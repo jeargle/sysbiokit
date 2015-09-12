4c781795350875b999862ef690c90e607b3b8a26
John Eargle <jeargle@gmail.com>
2015-09-11 23:13:53 -0500

    plot test and stub for regulated gene product

--- src/sysbiokit.py
+++ src/sysbiokit.py
@@ -4,12 +4,30 @@
 import sys
 import json
 
-import matplotlib
-import sympy
+import matplotlib.pyplot as plt
+import numpy as np
+# import sympy
 
 
 
+def time_plot(duration):
+    x = np.arange(0, duration, 0.1)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.show()
 
-        
+    return
+
+
+class Product():
+
+    def __init__(self):
+        pass
+
+    def plot(self):
+        pass
+
+
+
 if __name__=='__main__':
-    pass
+    time_plot(30)
