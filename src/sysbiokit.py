11fcd6e69fda2f18295a7bc70f21052d28e2b177
John Eargle <jeargle@gmail.com>
2015-09-12 17:50:02 -0500

    added simply regulated gene product

--- src/sysbiokit.py
+++ src/sysbiokit.py
@@ -12,22 +12,53 @@ import numpy as np
 
 def time_plot(duration):
     x = np.arange(0, duration, 0.1)
-    y = np.sin(x)
+    y = np.sin(x) * np.exp(-x/20.0) * 5.0
     plt.plot(x, y)
     plt.show()
 
     return
 
 
-class Product():
+class SimpleProduct():
 
-    def __init__(self):
-        pass
+    def __init__(self, name, const_rate=0.0, self_rate=0.0, product_type='protein'):
+        self.name = name
+        self.const_rate = const_rate
+        self.self_rate = self_rate
+        self.product_type = product_type
 
+    def report(self):
+        print '%s %s' % (self.product_type, self.name)
+        if self.self_rate > -0.00001 and self.self_rate < 0.00001:
+            print '  dX(t)/dt = %.2f' % (self.const_rate)
+        else:
+            if self.self_rate > 0.0:
+                print '  dX(t)/dt = %.2f + %.2fX' % (self.const_rate, self.self_rate)
+            else:
+                print '  dX(t)/dt = %.2f - %.2fX' % (self.const_rate, -self.self_rate)
+            print '  steady state = %.2f' % (-self.const_rate/self.self_rate)
+            print '  reaction time = %.2f' % (-np.log(2.0)/self.self_rate)
+            
+        
     def plot(self):
-        pass
+        if self.self_rate > -0.00001 and self.self_rate < 0.00001:
+            t = np.arange(0, 10, 0.1)
+            y = self.const_rate * t
+        else:
+            steady_rate = -self.const_rate/self.self_rate
+            rx_t = -np.log(2.0)/self.self_rate
+            duration = rx_t*4.0
+            t = np.arange(0, duration, duration/50.0)
+            y = steady_rate * (1.0 - np.exp(self.self_rate*t))
+        plt.plot(t, y)
+        plt.show()
 
 
 
 if __name__=='__main__':
-    time_plot(30)
+    # time_plot(30)
+    sp1 = SimpleProduct('Y', 2.0, -0.5)
+    sp1.report()
+    sp1.plot()
+
+
